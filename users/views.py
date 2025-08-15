371214a215a226322042bc0f2f542cdc79caaa44
Ankur Paudel <ankur@paudel.me>
2025-08-15 15:09:56 +0100

    feat: setup login/logout views

--- users/views.py
+++ users/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView as _DjangoLoginView, LogoutView as _DjangoLogoutView
+
+from users.forms import LoginForm
 
 # Create your views here.
+
+
+class LoginView(_DjangoLoginView):
+    template_name = "users/pages/login.html"
+    form_class = LoginForm
+
+
+class LogoutView(_DjangoLogoutView):
+    pass
